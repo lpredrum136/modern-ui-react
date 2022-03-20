1eb3affffe04537e87e4cf6af9092d1ce031a79c
Henry Nguyen <nguyenhai1591@gmail.com>
2022-03-20 22:09:38 +1000

    done blog

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -1,5 +1,30 @@
-const Article = () => {
-  return <div>Article</div>
+import './index.css'
+
+const Article = ({
+  imgUrl,
+  date,
+  text
+}: {
+  imgUrl: string
+  date: string
+  text: string
+}) => {
+  return (
+    <div className="h-full flex flex-col bg-footer">
+      <div className="bg-gpt3">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="flex flex-col justify-between px-6 py-4 ">
+        <div>
+          <p className="article-paragraph">{date}</p>
+          <h3 className="text-2xl font-extrabold leading-7 text-white mb-20 cursor-pointer">
+            {text}
+          </h3>
+        </div>
+        <p className="article-paragraph cursor-pointer">Read Full Article</p>
+      </div>
+    </div>
+  )
 }
 
 export default Article
--- src/containers/blog/index.tsx
+++ src/containers/blog/index.tsx
@@ -1,5 +1,37 @@
+import blog01 from '../../assets/blog01.png'
+import blog02 from '../../assets/blog02.png'
+import blog03 from '../../assets/blog03.png'
+import blog04 from '../../assets/blog04.png'
+import blog05 from '../../assets/blog05.png'
+import { Article } from '../../components'
+
+const defaultProps = {
+  date: 'Sep 26, 2021',
+  text: 'GPT-3 and Open AI is the future. Let us exlore how it is?'
+}
+
 const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <div className="section__padding flex flex-col" id="blog">
+      <div className="text-left mb-20">
+        <h1 className="gradient__text text-6xl leading-[75px] font-extrabold">
+          A lot is happening.
+          <br />
+          We are blogging about it.
+        </h1>
+      </div>
+      <div className="flex">
+        <div className="mr-8">
+          <Article imgUrl={blog01} {...defaultProps} />
+        </div>
+        <div className="grid grid-cols-2 gap-8">
+          {[blog02, blog03, blog04, blog05].map((blog, index) => (
+            <Article imgUrl={blog} key={index} {...defaultProps} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Blog
